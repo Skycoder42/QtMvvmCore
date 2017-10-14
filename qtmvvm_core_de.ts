cd983f4579ee1168a85e7b2642e31f4b2193436b
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-10-15 00:39:55 +0200

    update ts

--- qtmvvm_core_de.ts
+++ qtmvvm_core_de.ts
@@ -4,55 +4,55 @@
 <context>
     <name>CoreApp</name>
     <message>
-        <location filename="coreapp.cpp" line="77"/>
+        <location filename="coreapp.cpp" line="+77"/>
         <source>Invalid Arguments</source>
         <translation>Ungültige Argumente</translation>
     </message>
     <message>
-        <location filename="coreapp.cpp" line="107"/>
+        <location line="+30"/>
         <source>Usage</source>
         <translation>Verwendung</translation>
     </message>
     <message>
-        <location filename="coreapp.cpp" line="114"/>
+        <location line="+7"/>
         <source>Version</source>
         <translation>Version</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="12"/>
-        <location filename="coremessage.cpp" line="58"/>
-        <location filename="coremessage.cpp" line="80"/>
-        <location filename="coremessage.cpp" line="102"/>
+        <location filename="coremessage.cpp" line="+12"/>
+        <location line="+46"/>
+        <location line="+22"/>
+        <location line="+22"/>
         <source>Ok</source>
         <translation>Ok</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="34"/>
+        <location line="-68"/>
         <source>Yes</source>
         <translation>Ja</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="35"/>
+        <location line="+1"/>
         <source>No</source>
         <translation>Nein</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="103"/>
+        <location line="+68"/>
         <source>Cancel</source>
         <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="147"/>
+        <location line="+44"/>
         <source>%1&lt;br/&gt;Version %2</source>
         <translation>%1&lt;br/&gt;Version %2</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="152"/>
+        <location line="+5"/>
         <source>&lt;br/&gt;&lt;br/&gt;Developed by: &lt;a href=&quot;%1&quot;&gt;%2&lt;/a&gt;</source>
         <translation>&lt;br/&gt;&lt;br/&gt;Entwickelt von: &lt;a href=&quot;%1&quot;&gt;%2&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="coremessage.cpp" line="157"/>
+        <location line="+5"/>
         <source>&lt;br/&gt;&lt;br/&gt;Built with &lt;a href=&quot;https://www.qt.io&quot;&gt;Qt %2&lt;/a&gt;</source>
         <translation>&lt;br/&gt;&lt;br/&gt;Erstellt mit &lt;a href=&quot;https://www.qt.io&quot;&gt;Qt %2&lt;/a&gt;</translation>
     </message>
